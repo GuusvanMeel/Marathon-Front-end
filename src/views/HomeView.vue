b3dc0a678c017d038739055c5de3cb3b4cb6d7e2
GuusvanMeel <vanmeelguus@gmail.com>
2026-03-13 11:02:10 +0100

    added fetch to backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from "vue"
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const marathons = ref<any[]>([])
+
+onMounted(async () => {
+  const response = await fetch("/api/marathons")
+  const data = await response.json()
+  marathons.value = data
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Marathons</h1>
+
+    <ul>
+      <li v-for="marathon in marathons" :key="marathon.id">
+        {{ marathon.name }} - {{ marathon.location }} ({{ marathon.distance }} km)
+      </li>
+    </ul>
   </main>
 </template>
